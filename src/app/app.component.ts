73731af4514a6ebc758eb362ee751c9c1d3787df
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-03-15 18:47:46 -0300

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './view/component/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  template: `<router-outlet></router-outlet>`
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'santa-fe-front';
