17c85b0969ee55b3d57f88f7a0e1f61a6407cfeb
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-03-15 22:15:20 -0300

    making buttons for CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './view/component/header/header.component';
+import { ContainerCrudComponent } from './view/component/container-crud/container-crud.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterOutlet],
+  imports: [CommonModule, HeaderComponent, ContainerCrudComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
---
+++ src/app/view/component/container-crud/container-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './container-crud.component.html',
+  styleUrls: ['./container-crud.component.css'],
+})
+export class ContainerCrudComponent {}
